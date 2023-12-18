88032a686465984f0157b9585c88dc63b4392c0c
Gabriel Silva <gabrielp_lacerda@hotmail.com>
2023-12-18 17:36:54 -0300

    add build vite

--- src/pages/home/initialSection/Index.tsx
+++ src/pages/home/initialSection/Index.tsx
@@ -4,7 +4,7 @@ import Logo from "../../../assets/logo.png";
 export const Home = () => {
   return (
     <S.InitialHomePage id="home">
-      <img src={Logo} alt="" />
+      <img src={Logo} alt="logo" />
       <a href="#occupancyArea">meus serviços</a>
     </S.InitialHomePage>
   );
